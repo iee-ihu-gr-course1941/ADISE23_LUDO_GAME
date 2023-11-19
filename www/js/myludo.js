447a27ddd45ebd4975ba639df7e22d325129ac22
Dimitris Giouvanakis <dimitris121997@gmail.com>
2023-11-19 14:21:13 +0200

    css 1

--- www/js/myludo.js
+++ www/js/myludo.js
@@ -1,7 +1,17 @@
+var me={token:null,piece_color:null};
+var game_status={};
+var board={};
+var last_update=new Date().getTime();
+var timer=null;
+
+
 $(function(){
     draw_empty_board();
     fill_board();
+    $('#ludo_login').click(login_to_game);
+
     $('#ludo_reset').click(reset_board);
+    game_status_update();
 });
 
 function draw_empty_board() {
@@ -9,7 +19,7 @@ function draw_empty_board() {
 
     for (var i = 11; i > 0; i--) {
         t += '<tr>';
-        for (var j = 1; j < 12; j++) {
+        for (var j = 1; j  < 12; j++) {
             t += '<td class="ludo_square" id="square_' + j + '_' + i + '">' + j + ',' + i + '</td>';
         }
         t += '</tr>';
@@ -42,9 +52,78 @@ function fill_board_by_data(data) {
 		var o = data[i];
 		var id = '#square_'+ o.x +'_' + o.y;
          var c = (o.piece!=null)?  o.piece:'';
-
-      // $(id).addClass(o.b_color+'_square').html(c);
-	  var im =(o.piece!=null)?'<img class="piece" src="images/'+c+'.png">':'';
+var im =(o.piece!=null)?'<img class="piece" src="images/'+c+'.png">':'';
 	 	$(id).addClass(o.b_color+'_square').html(im);
-	}
-}
+        
+	}}
+
+    function login_to_game() {
+        if($('#username').val()=='') {
+            alert('You have to set a username');
+            return;
+        }
+        var p_color = $('#pcolor').val();
+        draw_empty_board(p_color);
+        fill_board();
+        
+        $.ajax({url: "ludo.php/players/"+p_color, 
+                method: 'PUT',
+                dataType: "json",
+                headers: {"X-Token": me.token},
+                contentType: 'application/json',
+                data: JSON.stringify( {username: $('#username').val(), piece_color: p_color}),
+                success: login_result,
+                error: login_error});
+    }
+
+
+
+    function login_result(data) {
+        me = data[0];
+        $('#game_initializer').hide();
+       update_info();
+          game_status_update();
+    }
+    
+    
+    function login_error(data,y,z,c) {
+        var x = data.responseJSON;
+        alert(x.errormesg);
+    }
+  
+   
+    function game_status_update() {
+	        clearTimeout(timer);
+        $.ajax({url: "ludo.php/status/", success: update_status,headers: {"X-Token": me.token} });
+    }
+    
+    function update_status(data) {
+        last_update=new Date().getTime();
+        var game_stat_old = game_status;
+        game_status=data[0];
+        update_info();
+       // clearTimeout(timer);
+       // if(game_status.p_turn==me.piece_color &&  me.piece_color!=null) {
+    //        x=0;
+    //        // do play
+   //         if(game_stat_old.p_turn!=game_status.p_turn) {
+  //              fill_board();
+    //        }
+    //        $('#move_div').show(1000);
+     //       timer=setTimeout(function() { game_status_update();}, 15000);
+     //   } else {
+      //      // must wait for something
+      //      $('#move_div').hide(1000);
+      //      timer=setTimeout(function() { game_status_update();}, 4000);
+        }
+
+        function update_info(){
+            $('#game_info').html("I am Player: "+me.piece_color+", my name is "+me.username +'<br>Token='+me.token+'<br>Game state: '+game_status.status+', '+ game_status.p_turn+' must play now.');
+            
+            
+        }
+         
+  
+
+
+    
