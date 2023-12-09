97711e6a0c576dd7e6beefabac849f4a9cbbfac5
margaretbluee <margarita.iliopoulou@hotmail.com>
2023-12-09 09:58:24 +0200

    dice

--- www/index.php
+++ www/index.php
@@ -11,7 +11,58 @@
     <section id='main_body'>
 
         <div id='ludo_board'></div>
-    
+        <div id="background"></div>
+<div id="wrapper">
+  <p id="point">0</p>
+  <button id="play">Play</button>
+  <div id="platform">
+    <div id="dice">
+      <div class="side front">
+        <div class="dot center"></div>
+      </div>
+      <div class="side front inner"></div>
+      <div class="side top">
+        <div class="dot dtop dleft"></div>
+        <div class="dot dbottom dright"></div>
+      </div>
+      <div class="side top inner"></div>
+      <div class="side right">
+        <div class="dot dtop dleft"></div>
+        <div class="dot center"></div>
+        <div class="dot dbottom dright"></div>
+      </div>
+      <div class="side right inner"></div>
+      <div class="side left">
+        <div class="dot dtop dleft"></div>
+        <div class="dot dtop dright"></div>
+        <div class="dot dbottom dleft"></div>
+        <div class="dot dbottom dright"></div>
+      </div>
+      <div class="side left inner"></div>
+      <div class="side bottom">
+        <div class="dot center"></div>
+        <div class="dot dtop dleft"></div>
+        <div class="dot dtop dright"></div>
+        <div class="dot dbottom dleft"></div>
+        <div class="dot dbottom dright"></div>
+      </div>
+      <div class="side bottom inner"></div>
+      <div class="side back">
+        <div class="dot dtop dleft"></div>
+        <div class="dot dtop dright"></div>
+        <div class="dot dbottom dleft"></div>
+        <div class="dot dbottom dright"></div>
+        <div class="dot center dleft"></div>
+        <div class="dot center dright"></div>
+      </div>
+      <div class="side back inner"></div>
+      <div class="side cover x"></div>
+      <div class="side cover y"></div>
+      <div class="side cover z"></div>
+    </div>
+  </div>
+</div>
+
         <div id='ui_Chat'>
             <a id='game_info'>
              
@@ -53,7 +104,8 @@
         <button id='players_reset' class='btn btn-primary'>ΤΟΟΟ ΚΟΥΜΠΙ NULL(100% ΔΟΥΛΕΥΕΙ)</button><br>
 
     </div>
-
+ 
+ 
 
 <?php
 
