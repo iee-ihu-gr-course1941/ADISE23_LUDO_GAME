080cd370239ec1f131f3df888920586e389abaf2
margaretbluee <margarita.iliopoulou@hotmail.com>
2023-12-20 16:49:06 +0200

    efygan ta tripla alert!

--- www/header.php
+++ www/header.php
@@ -25,8 +25,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <!-- Your custom JavaScript -->
-    <script src="js/myludo.js" ></script>
-    <script src="js/myludo.js", src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+ 
 </head>
 
 <body>
