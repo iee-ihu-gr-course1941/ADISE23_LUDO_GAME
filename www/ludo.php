c20433299065091194f79f8b0a3ffbd069bfade1
margaretbluee <margarita.iliopoulou@hotmail.com>
2023-12-25 09:13:57 +0200

    ludo.php blue green: win_pieces&roll_dice&highlight cases

--- www/ludo.php
+++ www/ludo.php
@@ -63,6 +63,16 @@ if(isset($_SERVER['HTTP_X_TOKEN'])) {
 				case 'rollR3':handle_roll_R3($method); break;
 				case 'rollR4':handle_roll_R4($method); break;
 
+				case 'rollG1':   handle_roll_G1($method); break;
+				case 'rollG2':handle_roll_G2($method); break;
+				case 'rollG3':handle_roll_G3($method); break;
+				case 'rollG4':handle_roll_G4($method); break;
+	  
+					case 'rollB1':handle_roll_B1($method); break;
+					case 'rollB2':handle_roll_B2($method); break;
+					case 'rollB3':handle_roll_B3($method); break;
+					case 'rollB4':handle_roll_B4($method); break;
+
 				case 'highlightY1':   handle_highlight_Y1($method); break;
 				case 'highlightY2':handle_highlight_Y2($method); break;
 				case 'highlightY3':handle_highlight_Y3($method); break;
@@ -73,8 +83,21 @@ if(isset($_SERVER['HTTP_X_TOKEN'])) {
 					case 'highlightR3':handle_highlight_R3($method); break;
 					case 'highlightR4':handle_highlight_R4($method); break;
 
+					case 'highlightG1':   handle_highlight_G1($method); break;
+					case 'highlightG2':handle_highlight_G2($method); break;
+					case 'highlightG3':handle_highlight_G3($method); break;
+					case 'highlightG4':handle_highlight_G4($method); break;
+		  
+						case 'highlightB1':handle_highlight_B1($method); break;
+						case 'highlightB2':handle_highlight_B2($method); break;
+						case 'highlightB3':handle_highlight_B3($method); break;
+						case 'highlightB4':handle_highlight_B4($method); break;
+
 	 case 'yellow_win_pieces':handle_yellow_win_pieces($method);break;
 	 case 'red_win_pieces':handle_red_win_pieces($method);break;
+	 case 'green_win_pieces':handle_green_win_pieces($method);break;
+	 case 'blue_win_pieces':handle_blue_win_pieces($method);break;
+
 	 case 'return_losers_home' : handle_losers($method);break;
 	 case 'timer' : handle_timer($method); break;
 	//case 'move_y':handle_move_y($method);
@@ -107,6 +130,16 @@ function handle_red_win_pieces($method) {
     }  else {header('HTTP/1.1 405 Method Not Allowed');}
 }
 
+function handle_green_win_pieces($method) {
+    if($method=='GET') {
+	 fill_green_win_pieces();
+      }  else {header('HTTP/1.1 405 Method Not Allowed');}
+}
+function handle_blue_win_pieces($method) {
+    if($method=='GET') {
+	 fill_blue_win_pieces();
+      }  else {header('HTTP/1.1 405 Method Not Allowed');}
+}
 
 function handle_delete_players($method) {
     if($method=='GET') {
@@ -237,6 +270,112 @@ function handle_delete_players($method) {
     
  }
  
+ function handle_roll_G1($method) {
+    if($method=='GET') {
+		roll_dice(1111);
+ 
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_highlight_G1($method) {
+    if($method=='GET') {
+ G1_highlight();
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_roll_G2($method) {
+    if($method=='GET') {
+		roll_dice(2222);
+ 
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_highlight_G2($method) {
+    if($method=='GET') {
+ G2_highlight();
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_roll_G3($method) {
+    if($method=='GET') {
+		roll_dice(3333);
+ 
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_highlight_G3($method) {
+    if($method=='GET') {
+ G3_highlight();
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_roll_G4($method) {
+    if($method=='GET') {
+		roll_dice(4444);
+ 
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_highlight_G4($method) {
+    if($method=='GET') {
+ G4_highlight();
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ 
+function handle_roll_B1($method) {
+    if($method=='GET') {
+		roll_dice(11);
+ 
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_highlight_B1($method) {
+    if($method=='GET') {
+ B1_highlight();
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_roll_B2($method) {
+    if($method=='GET') {
+		roll_dice(22);
+ 
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_highlight_B2($method) {
+    if($method=='GET') {
+ B2_highlight();
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_roll_B3($method) {
+    if($method=='GET') {
+		roll_dice(33);
+ 
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_highlight_B3($method) {
+    if($method=='GET') {
+ B3_highlight();
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_roll_B4($method) {
+    if($method=='GET') {
+		roll_dice(44);
+ 
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+ function handle_highlight_B4($method) {
+    if($method=='GET') {
+ B4_highlight();
+     }    else {header('HTTP/1.1 405 Method Not Allowed');}
+    
+ }
+
 
 function handle_board($method) {
     if($method=='GET') {
